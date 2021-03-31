b36f97f19a6a12e7a55a2d5f567e46c7f7998fd9
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-03-31 11:47:27 +0200

    added submit button

--- app.js
+++ app.js
@@ -2,22 +2,37 @@ const PositiveMessage = () => <p>Możesz obejrzeć film. Zapraszamy!</p>;
 
 const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu, jeśli nie masz skończonych 16 lat.</p>;
 
-class CheckboxAgeConfirmation extends React.Component {
+class TicketShop extends React.Component {
   state = {
     isConfirmed: false,
+    isFormSubmitted: false,
   };
 
-  handleCheckboxChange = () => {
+  handleChange = () => {
     this.setState({
       isConfirmed: !this.state.isConfirmed,
+      isFormSubmitted: false,
     });
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.isFormSubmitted) {
+      this.setState({
+        isFormSubmitted: true,
+      });
+    }
+  };
+
   displayMessage = () => {
-    if (this.state.isConfirmed) {
-      return <PositiveMessage />;
+    if (this.state.isFormSubmitted) {
+      if (this.state.isConfirmed) {
+        return <PositiveMessage />;
+      } else {
+        return <NegativeMessage />;
+      }
     } else {
-      return <NegativeMessage />;
+      return null;
     }
   };
 
@@ -25,12 +40,16 @@ class CheckboxAgeConfirmation extends React.Component {
     return (
       <>
         <h1>Kup bilet na horror roku!</h1>
-        <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed} />
-        <label htmlFor="age">Mam conajmniej 16 lat</label>
+        <form onSubmit={this.handleSubmit}>
+          <input type="checkbox" id="age" onChange={this.handleChange} checked={this.state.isConfirmed} />
+          <label htmlFor="age">Mam conajmniej 16 lat</label>
+          <br />
+          <button type="submit">Kup bilet</button>
+        </form>
         {this.displayMessage()}
       </>
     );
   }
 }
 
-ReactDOM.render(<CheckboxAgeConfirmation />, document.querySelector(`#root`));
+ReactDOM.render(<TicketShop />, document.querySelector(`#root`));
